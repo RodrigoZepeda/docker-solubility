181885acc8a809f96e4be28d2d7ceb7913f28747
RodrigoZepeda <rzepeda17@gmail.com>
2019-05-17 14:06:36 -0500

    Added error messages to python script for predicting solubility

--- Dockerfile
+++ Dockerfile
@@ -5,18 +5,19 @@
 #Download conda
 FROM continuumio/miniconda3
 
-#Set version
-LABEL version="0.1"
-LABEL maintainer="Rodrigo Zepeda <rzepeda17@gmail.com>"
+# Labels.
+#https://medium.com/@chamilad/lets-make-your-docker-image-better-than-90-of-existing-ones-8b1e5de950d
+LABEL org.label-schema.schema-version="1.0"
+#LABEL org.label-schema.build-date=$BUILD_DATE
+LABEL org.label-schema.name="rodrigozepeda/docker-solubility-v1"
+LABEL org.label-schema.description="Machine Learning models for predicting molecular solubility."
+LABEL org.label-schema.url="https://github.com/RodrigoZepeda/docker-solubility-v1"
+LABEL org.label-schema.version="0.1"
+LABEL org.label-schema.docker.cmd="docker run -it -v <PATH TO FILE YOU WANT TO WORK ON>:/data docker-solubility-v1"
 
 # Set environment variables
 ENV LANG en_US.UTF-8
 ENV LANGUAGE en_US:en
-
-#Set working directory
-COPY input_files /usr/src/input_files
-COPY models /usr/src/models
-COPY scripts /usr/src/scripts
 
 #Update conda
 RUN conda update conda
@@ -35,6 +36,11 @@ RUN yes "yes" | conda install -n env -c deepchem -c rdkit -c conda-forge -c omni
 
 #Clean conda after installation
 RUN yes "yes" | conda clean --all
+
+#Set files
+COPY input_files /usr/src/input_files
+COPY models /usr/src/models
+COPY scripts /usr/src/scripts
 
 #Run Python scripts
 CMD ["python","/usr/src/scripts/PredictionModelv01.py" ]
--- scripts/PredictionModelv01.py
+++ scripts/PredictionModelv01.py
@@ -1,20 +1,48 @@
 '''
-Runs
+Runs prediction model for graph convolution
+Rodrigo Zepeda rzepeda17@gmail.com
+
 '''
 import time
-import deepchem as dc
 import pandas as pd
+import sys
+import warnings
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
+#Importing deepchem throws a numpy warning
+sys.stderr = None            # suppress deprecation warning
+import deepchem as dc
+sys.stderr = sys.__stderr__  # restore stderr
+
 from deepchem.models import GraphConvModel
 from deepchem.utils.save import load_from_disk
 from rdkit import Chem
 
+#Tell user which model we are running
+print('Predicting with Graph Convolution Model...')
+
 #Featurize molecules
 featurizer = dc.feat.ConvMolFeaturizer()
-model = GraphConvModel.load_from_dir("/usr/src/models/graph_convolution")
+
+#Read molecule model from folder
+try:
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore",category=UserWarning)
+        model = GraphConvModel.load_from_dir("/usr/src/models/graph_convolution")
+except:
+    sys.exit('Unable to find graph convolution model at "/usr/src/models/graph_convolution"')
 
 #Read dataset to predict
-newsmiles = pd.read_csv('/data/To_predict.csv')
-mols = [Chem.MolFromSmiles(s) for s in newsmiles.loc[:,"Smiles"]]
+try:
+    newsmiles = pd.read_csv('/data/To_predict.csv')
+except:
+    sys.exit('Unable to read "To_predict.csv" from "/data" directory')
+
+try:
+    mols = [Chem.MolFromSmiles(s) for s in newsmiles.loc[:,"Smiles"]]
+except:
+    sys.exit('Unable to read "Smiles" column from "To_predict.csv"')
 
 #Featurize data
 x = featurizer.featurize(mols)
@@ -25,4 +53,13 @@ predicted_solubility = model.predict_on_batch(x)
 #Convert to dataframe
 mydf = pd.concat([newsmiles, pd.DataFrame(predicted_solubility)], axis = 1)
 mydf.columns = ["Smile","Predicted Solubility"]
-mydf.to_csv('/data/PredictedGraphConvolution ' + time.ctime() + '.csv', index=False)
+
+#Get file name
+fname = 'PredictedGraphConvolution ' + time.ctime() + '.csv'
+
+#Write dataset
+try:
+    mydf.to_csv('/data/' + fname, index=False)
+    print('Model saved as "' + fname + '" on "/data" directory')
+except:
+    sys.exit('Unable to write "' + fname + '" on "/data" directory')
