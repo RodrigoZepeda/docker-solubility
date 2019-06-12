56764b258fa050085e6b7f12c9aafeb23b5120a6
RodrigoZepeda <rzepeda17@gmail.com>
2019-06-12 10:32:31 -0500

    fixed untracked files

--- data_analysis/built_models/notifyending.py
+++
@@ -1,5 +0,0 @@
-import json
-import telegram
-def notify_ending(message):
-    bot = telegram.Bot(token='884880200:AAFrIUC-f4Z_0J_koQGX8dQl5qjmuTG7XS8')
-    bot.sendMessage(chat_id='358528401', text=message)
