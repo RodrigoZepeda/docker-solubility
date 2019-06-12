ea36a6d30eafb1ac51f2cf1f0356c7d70668f53a
RodrigoZepeda <rzepeda17@gmail.com>
2019-06-12 13:18:30 -0500

    Added created models; added description for rf

--- data_analysis/built_models/RandomForest.py
+++ data_analysis/built_models/RandomForest.py
@@ -24,9 +24,6 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import unicode_literals
 
-import os
-#os.chdir("/home/rod/Dropbox/Quimica/Analysis/ANalisis/Borradores")
-
 import numpy as np
 np.random.seed(1342341)
 
@@ -39,14 +36,22 @@ from deepchem.models.sklearn_models import RandomForestRegressor, SklearnModel
 from deepchem.utils.save import load_from_disk
 
 import pandas as pd
-import subprocess
+
+#Count number of processors of server
+import multiprocessing
+cpus = multiprocessing.cpu_count()
 
 """
 DATASETS
 """
-
-#Read dataset
 dataset_file= "delaney-processed.csv"
+modeldir = "random_forest/"
+
+#Create directory if not exists
+if not os.path.exists(modeldir):
+    os.makedirs(modeldir)
+    
+#Read dataset
 dataset = load_from_disk(dataset_file)
 
 #Establish featurizer
@@ -79,20 +84,18 @@ train_dataset, valid_dataset, test_dataset = splitter.train_valid_test_split(
 MODEL BUILDING
 """
 
-# Fit
-metric = dc.metrics.Metric(dc.metrics.pearson_r2_score, np.mean)
+# Fit using median as median is kept by exponential (ln S)
+metric = dc.metrics.Metric(dc.metrics.pearson_r2_score, np.median)
 
 # Do setup for optimal parameter searching
-n_estimators       = 2**np.linspace(0,20,21, endpoint=True, dtype=int)
+n_estimators       = 2**np.linspace(0,5,6, endpoint=True, dtype=int)
 max_features       = "auto" #Empirical
 
 #Empty for dataframe
 estimators    = []
 train_results = []
 test_results  = []
-depths        = []
-splits        = []
-leafs         = []
+metrics = []
 
 for estimator in n_estimators:
 
@@ -103,26 +106,35 @@ for estimator in n_estimators:
                                          max_features = max_features,
                                          bootstrap = True,
                                          oob_score = False,
-                                         n_jobs = 32)
-        model = SklearnModel(sklmodel, "RandomForest/")
+                                         n_jobs = int(cpus/2))
+        model = SklearnModel(sklmodel, modeldir)
         model.fit(train_dataset)
 
         #Append trains cores and results
-        train_scores = model.evaluate(train_dataset, [metric])
-        train_results.append(list(train_scores.values())[0])
-        valid_scores = model.evaluate(valid_dataset, [metric])
-        test_results.append(list(valid_scores.values())[0])
+        train_scores  = model.evaluate(train_dataset, [metric, dc.metrics.Metric(dc.metrics.mae_score)])
+        train_results = np.concatenate((train_results, list(train_scores.values())))
+        valid_scores  = model.evaluate(valid_dataset, [metric, dc.metrics.Metric(dc.metrics.mae_score)])
+        test_results  = np.concatenate((test_results,list(valid_scores.values())))
 
+        #Append trains cores and results
+        predict_train = pd.DataFrame(model.predict(train_dataset), columns=['prediction']).to_csv(modeldir + "predict_train_" + str(estimator) + '.csv')
+        predict_valid = pd.DataFrame(model.predict(valid_dataset), columns=['prediction']).to_csv(modeldir + "predict_validation_" + str(estimator) + '.csv')
+        
         #Append measures
-        estimators.append(estimator)
+        estimators = np.concatenate((estimators, [estimator, estimator]))
+        metrics    = np.concatenate((metrics, ["r2","mse"]))
 
 # Fit trained model
 model.save()
 print("Dataframe for n_estimator selection")
 df = pd.DataFrame({'nestimators': estimators,
                    'train_scores': train_results,
-                   'result_scores': test_results})
-df.to_csv("RandomForestAnalysis.csv")
+                   'result_scores': test_results,
+                   'metric': metrics})
+df.to_csv(modeldir + "RandomForestAnalysis.csv", index= False)
 
-from notifyending import *
-notify_ending("Random Forest")
+try: 
+    from notifyending import *
+    notify_ending("Finished fitting random forest")
+except:
+    print("Random forest")
---
+++ data_analysis/model_data_sheets/RandomForest.md
@@ -0,0 +1,26 @@
+Random Forest
+================
+
+## Model Description
+
+Here goes the model explanation
+
+### Model parameters and code
+
+Hola bola here should be the parameters
+
+## Model metrics
+
+![](RandomForest_files/figure-gfm/unnamed-chunk-1-1.png)<!-- -->
+
+## Example
+
+You can run the model via the following
+    command:
+
+    docker run --rm -v ~/PATH/TO_FILE/YOU_WANT_TO_WORK_ON/:/data docker-solubility RandomForest
+
+## See also
+
+  - [User’s
+    manual](https://github.com/RodrigoZepeda/docker-solubility/blob/master/Manual.md)
