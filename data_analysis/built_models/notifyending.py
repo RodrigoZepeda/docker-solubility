c2e2461e194261a5e94f12456c756e1263607060
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2019-06-13 15:40:22 -0500

    Delete notifyending.py

--- data_analysis/built_models/notifyending.py
+++
@@ -1,5 +0,0 @@
-import json
-import telegram
-def notify_ending(message):
-    bot = telegram.Bot(token='884880200:AAFXIlerG9cVfGXGER_FaEPdKDW92o1W_b8')
-    bot.sendMessage(chat_id='358528401', text=message)
